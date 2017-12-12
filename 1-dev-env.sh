70605ec98c020e1de55682e759efb7e5c9f221a7
Eric Kang <erickangatms@outlook.com>
2017-12-12 10:36:07 -0800

    .

--- 1-dev-env.sh
+++ 1-dev-env.sh
@@ -2,7 +2,7 @@
 
 # Shortcut: register Run Selected Text in Active Terminal to F6
 
-# init dev environment
+# init dev / troubleshooting environment
 clear
 docker-compose up -d
 
@@ -12,6 +12,10 @@ sleep 15
 # run database test
 clear
 docker exec -ti dbtest sh -c "cd /opt/slacker-test && slacker"
+
+#optional: view test spec
+clear
+docker exec -ti dbtest sh -c "cat /opt/slacker-test//spec/website/insertCustomerOrder.rb"
 
 
 # reset
